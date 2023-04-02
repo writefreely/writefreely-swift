b286634423f0dc10b2ecbcdacae8e4a507d88d53
Angelo Stavrow <angelo@droppedbits.com>
2023-04-02 08:01:04 -0400

    Rename test class

--- Tests/WriteFreelyTests/WriteFreelyClientTests.swift
+++ Tests/WriteFreelyTests/WriteFreelyClientTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import WriteFreely
 
-final class WriteFreelyTests: XCTestCase {
+final class WriteFreelyClientTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
