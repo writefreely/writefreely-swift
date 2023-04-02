56483157251d35c7f22e6e741b74c0bd27f471c9
Angelo Stavrow <angelo@droppedbits.com>
2023-04-02 08:01:27 -0400

    Add test utilities for network task mocking

---
+++ Tests/WriteFreelyTests/TestUtils.swift
@@ -0,0 +1,37 @@
+import Foundation
+import WriteFreely
+
+final class MockURLSessionDataTask: URLSessionDataTaskProtocol {
+    private (set) var resumeWasCalled = false
+
+    func resume() {
+        resumeWasCalled = true
+    }
+}
+
+final class MockURLSession: URLSessionProtocol {
+    var nextDataTask = MockURLSessionDataTask()
+    var nextData: Data?
+    var nextError: Error?
+    
+    private (set) var lastRequest: URLRequest?
+    
+    func successURLResponse(url: URL) -> URLResponse {
+        return HTTPURLResponse(
+            url: url,
+            statusCode: 200,
+            httpVersion: "HTTP/1.1",
+            headerFields: nil
+        )!
+    }
+
+    func dataTask(with request: URLRequest, completionHandler: @escaping DataTaskResult) -> URLSessionDataTaskProtocol {
+        lastRequest = request
+        completionHandler(
+            nextData,
+            successURLResponse(url: (request.url ?? URL(string: "https://example.com")!)),
+            nextError
+        )
+        return nextDataTask
+    }
+}
